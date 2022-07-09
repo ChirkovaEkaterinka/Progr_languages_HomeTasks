818751d3f83188da08288fd092f69d1876a3d2ad
Ekaterina <kartina_dzr@mail.ru>
2022-07-09 15:06:33 +0300

    Add task 4

---
+++ HomeTask_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напиишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N //
+Console.Write("Input number N = ");
+uint a = uint.Parse(Console.ReadLine());
+uint i = 1;
+while(i <= a)
+{
+    if(i % 2 == 0)
+    {
+    Console.WriteLine(i + " ");
+}
+i++;
+}
