092084c6ab7a7f2946bd19723450161c03aee546
Ekaterina <kartina_dzr@mail.ru>
2022-07-09 14:46:52 +0300

    Add task 3

---
+++ HomeTask_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напиишите программу, которая на вход принимает число и выдает, является ли оно четным //
+Console.WriteLine("Input number = ");
+int a = int.Parse(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine("Введенное число - четное ");
+}
+else
+{
+    Console.WriteLine("Введенное число - НЕчетное ");
+}
