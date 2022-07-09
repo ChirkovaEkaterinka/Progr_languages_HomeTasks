b0060e0d365360cbf2134898f4757002f1713737
Ekaterina <kartina_dzr@mail.ru>
2022-07-09 14:43:11 +0300

    Add task 1

---
+++ HomeTask_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдает максимальное из этих чисел
+*/
+Console.WriteLine("Input first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("max = " + a); 
+}
+else
+{
+    Console.WriteLine("max = " + b);
+}
